b17ace338687173ca21f79e5a88ece1901b914cc
mnapthine <mnapthine@gmail.com>
2020-02-02 00:41:47 +0000

    Tidy up the styles for Text and some more in terms of docs for VisuallyHidden

--- src/pages/visually-hidden.tsx
+++ src/pages/visually-hidden.tsx
@@ -19,7 +19,6 @@ const VisuallyHiddenDocs = () => {
             <span>{`<`}</span>VisuallyHidden
           </SPAN>
         </H1>
-
         <P>
           Provides text for screen readers that is visually hidden so as to keep
           your design clean. Often referred to as <code>srOnly</code> elsewhere
@@ -32,34 +31,27 @@ const VisuallyHiddenDocs = () => {
   Hidden text that remains accessible and indexable.
 </VisuallyHidden>`}
         </CodeSample>
-
         <H2 vol={6}>What is it for?</H2>
-
         <P>
           Quite often in web design we find situations where context can be
           derived visually from the design of a page. Consider a typical
           &rsquo;See more&rsquo; link as an example.
         </P>
-
         <P>
           It&rsquo;s easy to scan the page and figure out where links go based
           on the surrounding text or imagary. But what if you cannot see the
           surrounding text or imagary?
         </P>
-
         <P>
           Well, in that case you might be using a screen reader. If that{" "}
           <em>is</em> the case; then you will likely be using a cool feature
           that allows you read out all links on a page. Cool huh?
         </P>
-
         <H2 vol={5}>Example: Crappy link text</H2>
-
         <P>
           Lest take a look at a typical scenario on the web; a &rsquo;See
           more&rsquo; link without any context.
         </P>
-
         <CodeSample>
           {`<h2>What is Climate Change?</h2>
 <p>Nasa has a great resource for kids/adults all about climate change.</p>
@@ -71,37 +63,33 @@ const VisuallyHiddenDocs = () => {
   <a href="https://climatekids.nasa.gov/climate-change-meaning/">See more...</a>
 </p>`}
         </CodeSample>
-
         <P>
           Assuming the page is littered with this type of crappy link, which is
-          usually the case, how does it read if we are using a screen reader?
+          usually the case, how does it read using a screen reader?
         </P>
-
         <Blockquote variant={2}>
           <P>See more... See more... See more... See more... See more...</P>
         </Blockquote>
-
         <P>
           Ahhh, crap! That&rsquo;s about as useful as expanding the worlds
-          airports in the wake of a climate emergency.
+          airports in the wake of a planetary climate emergency.
         </P>
-
         <P>
-          Yep, stupid crap like that is a reality for a lot of people trying to
-          navigate the interwebs. It is one of many things that will likley piss
-          them off when using websites that do not adhere to the Web Content
-          Accessibiliy Guidelines (WCAG), which covers this type of stuff.
+          Yep, this is a reality for a lot of people trying to navigate the web.
+          It is one of many things that will likley piss them off when using
+          websites that do not adhere to the{" "}
+          <a href="https://www.w3.org/TR/WCAG20/">
+            Web Content Accessibiliy Guidelines 2.0 (WCAG)
+          </a>
+          , which covers this type of stuff.
         </P>
-
         <H3 vol={4}>Fixing the problem</H3>
-
         <P>
           In the following example, screen readers will announce the entire link
           even though sighted users will simply see &rsquo;See more&rsquo; as{" "}
           <del>God intended</del>, I mean as your <ins>designer requested</ins>.
           😇
         </P>
-
         <CodeSample>
           {`<h2>What is Climate Change?</h2>
 <p>Nasa has a great resource for kids/adults all about climate change.</p>
@@ -116,7 +104,6 @@ const VisuallyHiddenDocs = () => {
   </a>
 </p>`}
         </CodeSample>
-
         <P>
           When choosing link text or building webpages we need awareness and
           empathy when it comes to how others will interact with our creations.
@@ -124,10 +111,8 @@ const VisuallyHiddenDocs = () => {
           you are the type of person that would refuse to install an access ramp
           to a building. Who does that? Not us!
         </P>
-
         {/* https://www.youtube.com/watch?v=HceSej_z8Ik
         https://developers.google.com/web/fundamentals/accessibility/how-to-review */}
-
         <P>
           Lets help you navigate the{" "}
           <abbr
@@ -138,7 +123,6 @@ const VisuallyHiddenDocs = () => {
           </abbr>
           , in this case we are looking at:
         </P>
-
         <Blockquote
           citeUrl="https://www.w3.org/TR/UNDERSTANDING-WCAG20/navigation-mechanisms-refs.html"
           cite="Link Purpose (In Context): Understanding Success Criterion 2.4.4"
@@ -162,12 +146,10 @@ const VisuallyHiddenDocs = () => {
             where they are.
           </P>
         </Blockquote>
-
         <P>
           But actually more specifically than that we are looking at the success
           criterion of 2.4.4 which states:
         </P>
-
         <Blockquote
           citeUrl="https://www.w3.org/TR/UNDERSTANDING-WCAG20/navigation-mechanisms-refs.html"
           cite="Link Purpose (In Context): Understanding Success Criterion 2.4.4"
@@ -180,6 +162,66 @@ const VisuallyHiddenDocs = () => {
             ambiguous to users in general.
           </P>
         </Blockquote>
+        <P>
+          FML. How many times did you have to read that? Me too! ;-) Yes, these
+          do tend to be a little wordy and walk the line in terms of plain and
+          simple language; especially if you consider beginners to web
+          development and we must...
+        </P>
+        <P>
+          The peeps over there do a great job; it&rsquo;s a tough task, we
+          empathise with them. Ultimatley they are trying to make these things
+          as exact as possible so as to remove ambiguity. This is essential for
+          all implementers, from developers to browser vendors. Handy from a
+          legal perspective too if you happen to be suing for non-compliance.
+        </P>
+        <P>
+          So it is exact but it somehow misses the &rsquo;why&rsquo;. We should
+          look for the &rsquo;why&rsquo;; accessibility then becomes less of a
+          &rsquo;tick box&rsquo; exercise and more thought provoking. Lets have
+          a go at rewriting it from the perspective of our glorious users!
+        </P>
+        <Blockquote cite="Shelley UI - Yes we are quoting ourselves.">
+          <H2 vol={4}>
+            Accessible Links: Good for your readers, good for{" "}
+            <abbr title="Search Engine Optimisation">SEO</abbr>
+          </H2>
+          <P>
+            When reading out links by themsleves, I can get a good idea of where
+            the link will take me or what it will do if I select it. Unless
+            it&rsquo;s something like a game of &rsquo;snap&rsquo; where it
+            would kindda of defeat the point of the game if I can see through
+            the card. Obvs. ;-)
+          </P>
+        </Blockquote>
+        <P>
+          Put yourself in the position of that screen reader user... Do you
+          really want to piss yourself off? Nah, don&rsquo;t do it! An inclusive
+          approach will lead to a better experience and superior SEO if you care
+          about such things.
+        </P>
+
+        <P>
+          If you are using a CMS then; for external links you can provide an
+          extra field to capture the hidden text and for internal pages you can
+          dynamically append the destination page title:
+        </P>
+        <CodeSample>
+          {`<a href="/great-link-text/">
+  More Information<VisuallyHidden>: How to write great link text.</VisuallyHidden>
+</a>`}
+        </CodeSample>
+        <P>
+          We really need a new breed of clever CMS, including link text tips and
+          decent framing of the &rsquo;why&rsquo;. Continual auditing
+          doesn&rsquo;t work; it is admirable but unless you are educating
+          authors you are just gonna have the same issues over and over.
+        </P>
+
+        <P>
+          Make your shizzle accessible - it&rsquo;s your duty as a master
+          builder ;-)
+        </P>
       </div>
     </DefaultLayout>
   );
