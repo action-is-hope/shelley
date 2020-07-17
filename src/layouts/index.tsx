2b73b2a30a41abae7a1f3128c8aea3de0a7a6031
mnapthine <mnapthine@gmail.com>
2020-07-17 13:48:18 +0100

    Tidy up the switcher a bit, more to do just a quick one.

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState, useEffect, ReactNode } from "react";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
 import { classes as index } from "./index.st.css";
 import { Theme as defaultTheme } from "../projects/default";
 import { ThemeBar } from "../projects/themeSelector";
-// import project from "../projects/default/project.st.css";
-// import { classes as basic } from "./themes/basic.st.css";
-// import { classes as light } from "../projects/default/light.st.css";
+import { classes as light } from "../projects/default/light.st.css";
 import { classes as dark } from "../projects/default/dark.st.css";
 import classnames from "classnames";
 
-const Header = () => (
+const Header = ({
+  currentTheme,
+  setCurrentTheme
+}: {
+  currentTheme: string;
+  setCurrentTheme: (theme: string) => void;
+}) => (
   <div className={index.navbar}>
     <div className={index.inner}>
       <h1 className={index.title}>
@@ -24,7 +28,7 @@ const Header = () => (
         </Link>
       </h1>
       <div className={index.controls}>
-        <ThemeBar />
+        <ThemeBar {...{ currentTheme, setCurrentTheme }} />
       </div>
     </div>
   </div>
@@ -45,30 +49,41 @@ const Footer = () => (
   </div>
 );
 
-const DefaultLayout = ({ children }: any) => (
-  <div className={defaultTheme}>
-    <>
-      <Helmet
-        title="Shelley - A Stylable User Interface"
-        meta={[
-          {
-            name: "description",
-            content:
-              "React UI lib: Create something beautiful from recycled body parts."
-          },
-          { name: "keywords", content: "sample, something" }
-        ]}
-        htmlAttributes={{
-          lang: "en"
-          // @todo fix so no FOUC
-          // className: classnames(defaultTheme, dark.root)
-        }}
-      />
-      <Header />
-      {children}
-      <Footer />
-    </>
-  </div>
-);
+const DefaultLayout = ({ children }: { children: ReactNode }) => {
+  // Theme state.
+  const [currentTheme, setCurrentTheme] = useState(dark.root);
+  useEffect(() => {
+    // Set the theme to stored value else dark as default.
+    setCurrentTheme(window.localStorage.getItem("currentTheme") || "dark");
+  }, []);
+
+  return (
+    <div>
+      <>
+        <Helmet
+          title="Shelley - A Stylable User Interface"
+          meta={[
+            {
+              name: "description",
+              content:
+                "React UI lib: Create something beautiful from recycled body parts."
+            },
+            { name: "keywords", content: "sample, something" }
+          ]}
+          htmlAttributes={{
+            lang: "en",
+            class: classnames(
+              defaultTheme,
+              currentTheme === "light" ? light.root : dark.root
+            )
+          }}
+        />
+        <Header {...{ currentTheme, setCurrentTheme }} />
+        {children}
+        <Footer />
+      </>
+    </div>
+  );
+};
 
 export default DefaultLayout;
--- src/projects/themeSelector.tsx
+++ src/projects/themeSelector.tsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import project from "./default/project.st.css";
-// import { classes as basic } from "./themes/basic.st.css";
 import light from "./default/light.st.css";
 import dark from "./default/dark.st.css";
-// import inputSelection from "./default/css/inputSelection.st.css";
 import { classes as inputSelectionCustom } from "./default/css/inputSelectionCustom.st.css";
 import InputSelection from "../components/InputSelection/InputSelection";
 import Icon from "../components/Icon/Icon";
@@ -17,20 +15,20 @@ const themes = {
 // This functions changes the application them by appending the selected
 // theme class name to the top of the document. In this application,
 // only a single theme is applied at a time.
+// @remove and dumify this so we can reuse it.
 export function changeTheme(name: keyof typeof themes) {
   localStorage.currentTheme = name;
   document.documentElement.className = themes[name].join(" ");
 }
 
-export function ThemeBar({ theme }: { theme?: string }) {
-  const [lightTheme, setLightTheme] = useState(false);
-
-  useEffect(() => {
-    const localTheme: any =
-      window.localStorage.getItem("currentTheme") || "dark";
-    localTheme && setLightTheme(localTheme === "light" ? true : false);
-    localTheme && changeTheme(localTheme);
-  }, []);
+export function ThemeBar({
+  currentTheme,
+  setCurrentTheme
+}: {
+  currentTheme: string;
+  setCurrentTheme: (theme: string) => void;
+}) {
+  const lightOn = currentTheme === "light";
 
   return (
     <InputSelection
@@ -42,20 +40,20 @@ export function ThemeBar({ theme }: { theme?: string }) {
           <path d="M16 8l-2.2-1.6 1.1-2.4-2.7-0.2-0.2-2.7-2.4 1.1-1.6-2.2-1.6 2.2-2.4-1.1-0.2 2.7-2.7 0.2 1.1 2.4-2.2 1.6 2.2 1.6-1.1 2.4 2.7 0.2 0.2 2.7 2.4-1.1 1.6 2.2 1.6-2.2 2.4 1.1 0.2-2.7 2.7-0.2-1.1-2.4 2.2-1.6zM8 13c-2.8 0-5-2.2-5-5s2.2-5 5-5 5 2.2 5 5-2.2 5-5 5z"></path>
         </Icon>
       }
-      defaultChecked={lightTheme}
+      defaultChecked={lightOn}
       className={classnames(inputSelectionCustom.darkLightToggle, {
-        [inputSelectionCustom.on]: lightTheme
+        [inputSelectionCustom.on]: lightOn
       })}
-      checked={lightTheme}
+      checked={lightOn}
       onKeyPress={event => {
         if (event.key === "Enter") {
-          setLightTheme(!lightTheme);
-          changeTheme(lightTheme ? "dark" : "light");
+          changeTheme(lightOn ? "dark" : "light");
+          setCurrentTheme(lightOn ? "dark" : "light");
         }
       }}
       onChange={() => {
-        setLightTheme(!lightTheme);
-        changeTheme(lightTheme ? "dark" : "light");
+        changeTheme(lightOn ? "dark" : "light");
+        setCurrentTheme(lightOn ? "dark" : "light");
       }}
       type="checkbox"
     />
