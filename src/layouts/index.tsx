5de43bfb515ef75a19fbebdbb514949170e34524
mnapthine <mnapthine@gmail.com>
2020-07-17 16:11:08 +0100

    Working through some Puma theme support.

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,18 +2,21 @@ import React, { useState, useEffect, ReactNode } from "react";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
 import { classes as index } from "./index.st.css";
-import { Theme as defaultTheme } from "../projects/default";
 import { ThemeBar } from "../projects/themeSelector";
+import { classes as project } from "../projects/default/project.st.css";
+
 import { classes as light } from "../projects/default/light.st.css";
 import { classes as dark } from "../projects/default/dark.st.css";
 import classnames from "classnames";
 
+type themeOptions = "light" | "dark";
+
 const Header = ({
   currentTheme,
-  setCurrentTheme
+  toggleTheme
 }: {
-  currentTheme: string;
-  setCurrentTheme: (theme: string) => void;
+  currentTheme: themeOptions;
+  toggleTheme: (theme: themeOptions) => void;
 }) => (
   <div className={index.navbar}>
     <div className={index.inner}>
@@ -28,7 +31,7 @@ const Header = ({
         </Link>
       </h1>
       <div className={index.controls}>
-        <ThemeBar {...{ currentTheme, setCurrentTheme }} />
+        <ThemeBar {...{ currentTheme, toggleTheme }} />
       </div>
     </div>
   </div>
@@ -49,13 +52,36 @@ const Footer = () => (
   </div>
 );
 
+const themes = {
+  dark: [project.root, dark.root],
+  light: [project.root, light.root]
+};
+
+// This functions changes the application them by appending the selected
+// theme class name to the top of the document. In this application,
+// only a single theme is applied at a time.
+// @remove and dumify this so we can reuse it.
+export function changeTheme(name: keyof typeof themes) {
+  localStorage.currentTheme = name;
+  document.documentElement.className = themes[name].join(" ");
+}
+
 const DefaultLayout = ({ children }: { children: ReactNode }) => {
   // Theme state.
-  const [currentTheme, setCurrentTheme] = useState(dark.root);
+  const [currentTheme, setCurrentTheme] = useState<themeOptions>("dark");
   useEffect(() => {
+    const theme: themeOptions =
+      window.localStorage.getItem("currentTheme") === "light"
+        ? "light"
+        : "dark";
     // Set the theme to stored value else dark as default.
-    setCurrentTheme(window.localStorage.getItem("currentTheme") || "dark");
+    setCurrentTheme(theme);
   }, []);
+
+  const toggleTheme = (name: themeOptions) => {
+    changeTheme(name);
+    setCurrentTheme(name);
+  };
 
   return (
     <div>
@@ -72,13 +98,10 @@ const DefaultLayout = ({ children }: { children: ReactNode }) => {
           ]}
           htmlAttributes={{
             lang: "en",
-            class: classnames(
-              defaultTheme,
-              currentTheme === "light" ? light.root : dark.root
-            )
+            class: classnames(themes[currentTheme])
           }}
         />
-        <Header {...{ currentTheme, setCurrentTheme }} />
+        <Header {...{ currentTheme, toggleTheme }} />
         {children}
         <Footer />
       </>
--- src/projects/themeSelector.tsx
+++ src/projects/themeSelector.tsx
@@ -1,35 +1,19 @@
 import React from "react";
-import project from "./default/project.st.css";
-import light from "./default/light.st.css";
-import dark from "./default/dark.st.css";
 import { classes as inputSelectionCustom } from "./default/css/inputSelectionCustom.st.css";
 import InputSelection from "../components/InputSelection/InputSelection";
 import Icon from "../components/Icon/Icon";
 import classnames from "classnames";
 
-const themes = {
-  dark: [project.classes.root, dark.classes.root],
-  light: [project.classes.root, light.classes.root]
-};
-
-// This functions changes the application them by appending the selected
-// theme class name to the top of the document. In this application,
-// only a single theme is applied at a time.
-// @remove and dumify this so we can reuse it.
-export function changeTheme(name: keyof typeof themes) {
-  localStorage.currentTheme = name;
-  document.documentElement.className = themes[name].join(" ");
-}
+type themeOptions = "light" | "dark";
 
 export function ThemeBar({
   currentTheme,
-  setCurrentTheme
+  toggleTheme
 }: {
-  currentTheme: string;
-  setCurrentTheme: (theme: string) => void;
+  currentTheme: themeOptions;
+  toggleTheme: (theme: themeOptions) => void;
 }) {
   const lightOn = currentTheme === "light";
-
   return (
     <InputSelection
       id="themeSelector"
@@ -45,16 +29,10 @@ export function ThemeBar({
         [inputSelectionCustom.on]: lightOn
       })}
       checked={lightOn}
-      onKeyPress={event => {
-        if (event.key === "Enter") {
-          changeTheme(lightOn ? "dark" : "light");
-          setCurrentTheme(lightOn ? "dark" : "light");
-        }
-      }}
-      onChange={() => {
-        changeTheme(lightOn ? "dark" : "light");
-        setCurrentTheme(lightOn ? "dark" : "light");
-      }}
+      onKeyPress={event =>
+        event.key === "Enter" && toggleTheme(lightOn ? "dark" : "light")
+      }
+      onChange={() => toggleTheme(lightOn ? "dark" : "light")}
       type="checkbox"
     />
   );
