4502b1c436461cfc3f22ff032385d3a2a09ff88b
Mark Napthine <mnapthine@gmail.com>
2022-09-12 21:47:06 +0100

    SB theme tidy.

--- .storybook/ShelleyTheme.js
+++ .storybook/ShelleyTheme.js
@@ -7,37 +7,16 @@ export default create({
   brandUrl: "https://shelley.earth",
   // brandImage: "",
 
-  // colorPrimary: brand.blue,
-  // colorSecondary: brand.blueDark,
-
-  // appBg: "#171a23",
-  // appContentBg: brand.white,
-
-  // barTextColor: brand.blueDark,
-  // barSelectedColor: brand.blueDark,
-
   // UI
-  // appBg: "white",
   appContentBg: "#f1f1f1",
-  // appBorderColor: "grey",
-  // appBorderRadius: 4,
-
-  // Typography
-  // fontBase: "Helvetica Neue, Helvetica, Arial, sans-serif",
-  fontCode: "monospace",
 
   // Text colors
   textColor: "#cdcfd9",
   textInverseColor: "#333",
 
-  // Toolbar default and active colors
-  // barTextColor: "silver",
-  // barSelectedColor: "#32baba",
-  // barBg: "hotpink",
-
   // Form colors
-  inputBg: "white",
-  inputBorder: "silver",
-  inputTextColor: "black",
-  inputBorderRadius: 4,
+  // inputBg: "white",
+  // inputBorder: "silver",
+  // inputTextColor: "black",
+  // inputBorderRadius: 4,
 });
