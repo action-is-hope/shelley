ff95c3eaa0d04cc5ba91438414d7fa633b1b1851
mnapthine <mnapthine@gmail.com>
2023-02-24 16:37:46 +0000

    Adding types

---
+++ src/typings/shared-types.d.ts
@@ -0,0 +1,11 @@
+export type ValidationState = "valid" | "invalid";
+
+export interface Validation {
+  /** Whether the input should display its "valid" or "invalid" visual styling. */
+  validationState?: ValidationState;
+  /**
+   * Whether user input is required on the input before form submission.
+   * Often paired with the `necessityIndicator` prop to add a visual indicator to the input.
+   */
+  isRequired?: boolean;
+}
