3ce47e73b51fc2c0c534c92b01dee52261662a10
Bert <bertan@2creative.net>
2023-04-25 17:07:13 +0100

    adding tabs and functionality

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,27 +1,74 @@
-import React from "react";
+import { useTabListState } from "react-stately";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  useTab,
+  useTabList,
+  useTabPanel,
+  useFocusRing,
+  mergeProps,
+} from "react-aria";
 import { st, classes } from "./tabs.st.css";
 
-const tabsData = [
-  {
-    id: 1,
-    name: "Founding of Rome",
-    children: "Arma virumque cano, Troiae qui primus ab oris.",
-  },
-  {
-    id: 2,
-    name: "Monarchy and Republic",
-    children: "Senatus Populusque Romanus.",
-  },
-  { id: 3, name: "Empire", children: "Alea jacta est." },
-];
+function Tab({ item, state }) {
+  const ref = useRef();
+  const { tabProps } = useTab(item, state, ref);
 
-const Tabs = () => {
   return (
-    <div aria-label="History of Ancient Rome">
-      {(item: Tab) => <Item>{item.name}</Item>}
-      <TabPanels>{(item: Tab) => <Item>{item.children}</Item>}</TabPanels>
+    <div {...tabProps} ref={ref}>
+      {item.rendered}
     </div>
   );
-};
+}
 
-export default Tabs;
+function TabPanel({ state, ...props }) {
+  const ref = useRef();
+  const { tabPanelProps } = useTabPanel(props, state, ref);
+
+  return (
+    <div {...tabPanelProps} ref={ref}>
+      {state.selectedItem?.props.children}
+    </div>
+  );
+}
+
+export function Tabs(props) {
+  const state = useTabListState(props);
+  const ref = useRef();
+  const { tabListProps } = useTabList(props, state, ref);
+
+  const [activeTabStyle, setActiveTabStyle] = useState({
+    width: 0,
+    transform: "translateX(0)",
+  });
+
+  useEffect(() => {
+    const activeTab = ref.current.querySelector(
+      '[role="tab"][aria-selected="true"]'
+    );
+    setActiveTabStyle({
+      width: activeTab?.offsetWidth,
+      transform: `translateX(${activeTab?.offsetLeft}px)`,
+    });
+  }, [state.selectedKey]);
+
+  const { focusProps, isFocusVisible } = useFocusRing({
+    within: true,
+  });
+
+  return (
+    <div className="tabs">
+      <div className="tablist-container">
+        <div
+          className={`tab-selection ${isFocusVisible ? "focused" : ""}`}
+          style={{ zIndex: -1, ...activeTabStyle }}
+        />
+        <div {...mergeProps(tabListProps, focusProps)} ref={ref}>
+          {[...state.collection].map((item) => (
+            <Tab key={item.key} item={item} state={state} />
+          ))}
+        </div>
+      </div>
+      <TabPanel key={state.selectedItem?.key} state={state} />
+    </div>
+  );
+}
--- src/stories/UI/Tabs.examples.tsx
+++ src/stories/UI/Tabs.examples.tsx
@@ -1,1 +1,27 @@
 import * as React from "react";
+import Item from "../../components/Item/Item";
+import { Tabs } from "../../components/Tabs/Tabs";
+
+const tabsData = [
+  {
+    id: 1,
+    name: "Founding of Rome",
+    children: "Arma virumque cano, Troiae qui primus ab oris.",
+  },
+  {
+    id: 2,
+    name: "Monarchy and Republic",
+    children: "Senatus Populusque Romanus.",
+  },
+  { id: 3, name: "Empire", children: "Alea jacta est." },
+];
+
+export const Example1 = () => {
+  return (
+    <Tabs>
+      <Item title="Recent">Recent</Item>
+      <Item title="Favorites">Favorites</Item>
+      <Item title="All">All</Item>
+    </Tabs>
+  );
+};
