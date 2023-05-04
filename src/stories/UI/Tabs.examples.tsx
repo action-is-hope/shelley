68aa991b569d7d8e74867fb0647674d427f22235
Bert <bertan@2creative.net>
2023-05-04 11:47:24 +0100

    a few more examples

--- src/stories/UI/Tabs.examples.tsx
+++ src/stories/UI/Tabs.examples.tsx
@@ -2,26 +2,34 @@ import * as React from "react";
 import Item from "../../components/Item/Item";
 import Tabs from "../../components/Tabs/Tabs";
 
-const tabsData = [
-  {
-    id: 1,
-    name: "Founding of Rome",
-    children: "Arma virumque cano, Troiae qui primus ab oris.",
-  },
-  {
-    id: 2,
-    name: "Monarchy and Republic",
-    children: "Senatus Populusque Romanus.",
-  },
-  { id: 3, name: "Empire", children: "Alea jacta est." },
-];
-
 export const Example1 = () => {
   return (
     <Tabs>
       <Item title="Tab title 1">Tab description 1</Item>
       <Item title="Tab title 2">Tab description 2</Item>
       <Item title="Tab title 3">Tab description 3</Item>
+    </Tabs>
+  );
+};
+
+const tabs = [
+  { id: 1, title: "Tab 1", content: "Tab body 1" },
+  { id: 2, title: "Tab 2", content: "Tab body 2" },
+  { id: 3, title: "Tab 3", content: "Tab body 3" },
+];
+
+export const Example2 = () => {
+  return (
+    <Tabs aria-label="Dynamic tabs" items={tabs}>
+      {(item) => <Item title={item.title}>{item.content}</Item>}
+    </Tabs>
+  );
+};
+
+export const Example3 = () => {
+  return (
+    <Tabs aria-label="Dynamic tabs" items={tabs} isDisabled>
+      {(item) => <Item title={item.title}>{item.content}</Item>}
     </Tabs>
   );
 };
