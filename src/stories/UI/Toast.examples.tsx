adbb5265563b6c3705daa15b9c356491ec555b0b
Tamas Balint <balint.tamas87@gmail.com>
2023-05-18 22:36:33 +0100

    Add basic, priority and auto-dismiss examples

--- src/stories/UI/Toast.examples.tsx
+++ src/stories/UI/Toast.examples.tsx
@@ -1,16 +1,67 @@
 import { ToastProvider } from "../../components/Toast/ToastProvider";
 import Button from "../../components/Button/Button";
 
-export const ToastProviderExample = () => {
+export const ToastProviderBasic = () => {
   return (
     <ToastProvider>
       {(state) => (
-        <Button
-          onClick={() => state.add("Toast is done!")}
-          style={{ margin: "16px" }}
-        >
-          Add toasts
-        </Button>
+        <>
+          <Button
+            onClick={() => state.add("New toast")}
+            style={{ margin: "16px" }}
+          >
+            Add toast!
+          </Button>
+        </>
+      )}
+    </ToastProvider>
+  );
+};
+
+export const ToastProviderPriority = () => {
+  return (
+    <ToastProvider>
+      {(state) => {
+        console.log({ state });
+        return (
+          <>
+            <Button
+              onClick={() => state.add("Toasting...! (1)", { priority: 1 })}
+              style={{ margin: "16px" }}
+            >
+              Low priority(1) - info or success
+            </Button>
+            <Button
+              onClick={() => state.add("Toast is done! (2)", { priority: 2 })}
+              style={{ margin: "16px" }}
+            >
+              Medium priority(2) - warning
+            </Button>
+            <Button
+              onClick={() => state.add("Toast is burnt! (3)", { priority: 3 })}
+              style={{ margin: "16px" }}
+            >
+              High priority(3) - error
+            </Button>
+          </>
+        );
+      }}
+    </ToastProvider>
+  );
+};
+
+export const ToastProviderAutoDismiss = () => {
+  return (
+    <ToastProvider>
+      {(state) => (
+        <>
+          <Button
+            onClick={() => state.add("New toast", { timeout: 5000 })}
+            style={{ margin: "16px" }}
+          >
+            Add toast!
+          </Button>
+        </>
       )}
     </ToastProvider>
   );
