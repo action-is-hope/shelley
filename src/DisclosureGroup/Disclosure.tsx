8575769747c8320ade9fc802065bd0c949e89f6a
Bert <bertan@2creative.net>
2023-06-14 16:13:38 +0100

    adding a shelley button with icon some data-id attr

--- src/DisclosureGroup/Disclosure.tsx
+++ src/DisclosureGroup/Disclosure.tsx
@@ -1,8 +1,9 @@
 /** Disclosure.tsx */
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, ReactNode } from "react";
 import { st, classes } from "./disclosureGroup.st.css";
 import { Text } from "../Text/Text";
-// import Icon from "../Icon/Icon";
+import { Button } from "../Button";
+import AngleDown from "../icons/AngleDown";
 import { useId } from "@react-aria/utils";
 
 import useDisclosure from "./useDisclosure";
@@ -12,6 +13,10 @@ export interface DisclosureProps extends React.HTMLAttributes<HTMLElement> {
   id?: string;
   /** Disclosure title */
   title: string;
+  /** Provide your own icon for the Trigger */
+  triggerIcon?: ReactNode;
+  /** Data attribute for Cypress tests. */
+  "data-id"?: string;
 }
 
 const Disclosure: React.VFC<DisclosureProps> = ({
@@ -19,6 +24,8 @@ const Disclosure: React.VFC<DisclosureProps> = ({
   className,
   children,
   title,
+  triggerIcon = <AngleDown />,
+  "data-id": dataId,
   ...rest
 }) => {
   const id = useId(idProp);
@@ -46,25 +53,39 @@ const Disclosure: React.VFC<DisclosureProps> = ({
         className
       )}
       {...rest}
+      data-id={dataId ? `${dataId}--disclosure` : undefined}
     >
-      <button
+      <Button
         className={classes.trigger}
+        icon={triggerIcon}
+        variant={false}
+        tone={false}
         {...triggerProps}
         onClick={() => triggerProps.onClick()}
+        data-id={dataId ? `${dataId}--trigger` : undefined}
       >
-        <Text as="span" vol={3} className={classes.title}>
+        <Text
+          as="span"
+          vol={3}
+          className={classes.title}
+          data-id={dataId ? `${dataId}--title` : undefined}
+        >
           {title}
         </Text>
-        {/* <Icon icon="chevronDown" className={classes.triggerIcon} /> */}
-      </button>
+      </Button>
 
       <Text
         as="div"
         vol={2}
         className={classes.hiddenContent}
         {...contentProps}
+        data-id={dataId ? `${dataId}--hidden-content` : undefined}
       >
-        <div ref={hiddenContentRef} className={classes.content}>
+        <div
+          ref={hiddenContentRef}
+          className={classes.content}
+          data-id={dataId ? `${dataId}--content` : undefined}
+        >
           {children}
         </div>
       </Text>
