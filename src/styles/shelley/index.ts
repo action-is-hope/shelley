2a7fd9d8c3b1d0613bbb2ad18ebe11f9a8d9e372
Cristina Llamas <cristinallamasdevelopment@gmail.com>
2023-06-16 15:41:20 +0200

    working in storybook

--- src/styles/shelley/index.ts
+++ src/styles/shelley/index.ts
@@ -1,23 +1,23 @@
-// = Foundation Components
-import "./actionButton.st.css";
-import "./button.st.css";
-import "./blockquote.st.css";
-import "./helpText.st.css";
-import "./grid.st.css";
-import "./listBoxOption.st.css";
-import "./menuItem.st.css";
-import "./progressCircle.st.css";
-import "./tabs.st.css";
-import "./tableView.st.css";
-import "./text.st.css";
-import "./toast.st.css";
-
 // = Project
 import { classes as shelleyProject } from "./project.st.css";
-
+import { classes as dark } from "./themes/dark.st.css";
 // = Themes
 import { classes as light } from "./themes/light.st.css";
-import { classes as dark } from "./themes/dark.st.css";
+
+// = Foundation Components
+import "./actionButton.st.css";
+import "./blockquote.st.css";
+import "./button.st.css";
+import "./grid.st.css";
+import "./helpText.st.css";
+import "./listBoxOption.st.css";
+import "./menuItem.st.css";
+import "./progressBar.st.css";
+import "./progressCircle.st.css";
+import "./tableView.st.css";
+import "./tabs.st.css";
+import "./text.st.css";
+import "./toast.st.css";
 
 // = Main classname export
 export const Project = shelleyProject.root;
