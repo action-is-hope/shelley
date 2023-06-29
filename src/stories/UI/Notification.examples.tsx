51776eeedf9f7fb21fe3ea9db822fe09777be71c
Bert <bertan@2creative.net>
2023-06-29 17:58:04 +0100

    class names another example

--- src/stories/UI/Notification.examples.tsx
+++ src/stories/UI/Notification.examples.tsx
@@ -1,5 +1,23 @@
 import { Notification } from "../../indexLib";
 
 export const InlineNotification = () => {
-  return <Notification />;
+  return (
+    <Notification
+      role="alert"
+      title="Notification title"
+      subtitle="subtitle goes here"
+    />
+  );
+};
+
+export const InlineNotificationWithChildren = () => {
+  return (
+    <Notification
+      role="alert"
+      title="Notification title"
+      subtitle="subtitle goes here"
+    >
+      <p>Notification content goes here</p>
+    </Notification>
+  );
 };
