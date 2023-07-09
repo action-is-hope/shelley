fe97658c8768dacc4060e1bfcd100977a2a7378a
mnapthine <mnapthine@gmail.com>
2023-07-09 23:18:43 +0100

    fix: missed import, added some docs to the react def file.

--- src/react.d.ts
+++ src/react.d.ts
@@ -1,5 +1,25 @@
 import React from "react";
 
+/**
+ * forwardRef doesn't support generic parameters. Previously we have
+ * cast to the correct type in the component files.
+ * https://stackoverflow.com/questions/58469229/react-with-typescript-generics-while-using-react-forwardref
+ * EG:
+ *   const _Tabs = forwardRef(Tabs) as <T>(
+ *     props: TabsProps<T> & { ref?: Ref<HTMLElement> }
+ *   ) => ReactElement;
+ *
+ * Whilst we sometimes do still cast now augment React module type
+ * declarations overriding forwardRef with a new function overload
+ * type signature:
+ * https://stackoverflow.com/questions/58469229/react-with-typescript-generics-while-using-react-forwardref
+ *
+ * This means we can mostly just keep the components simplier than
+ * the above:
+ * const _Tabs = forwardRef(Tabs);
+ *
+ * Which is nicer and makes the components more consistant.
+ */
 // eslint-disable-line
 declare module "react" {
   function forwardRef<T, P = Record<string, unknown>>(
