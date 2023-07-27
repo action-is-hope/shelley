cffb4722a9ff6ade83c2d30193eb63b7cd270e25
Bert <1674135+2creative@users.noreply.github.com>
2023-07-27 09:27:08 +0100

    feat(component): Notification (#123)
    * adding the notification starter files
    
    * starting to build the notification component
    
    * class names another example
    
    * swapping the button to icon button
    
    * some dataIds
    
    * adding different notification types
    
    * adding the basic close functionality
    
    * adding the icons to start with
    
    * adding the icon condition and some basic styling
    
    * minor adjustments
    
    * some tests
    
    * tests
    
    * Adding another example
    
    * tests
    
    * tests
    
    * aria label close button and tests
    
    * close button in docs out of alignment
    
    * Adding docs, notification props table
    
    * some more docs
    
    * adding RTL
    
    * empty line
    
    * empty line
    
    * adding more rtl
    
    * matching the icon size to toast component
    
    * spelling
    
    * updating source examples
    
    ---------
    
    Co-authored-by: Bert <bertan@2creative.net>
    Co-authored-by: GaxZE <171002+GaxZE@users.noreply.github.com>
    Co-authored-by: Mark Napthine <mnapthine@gmail.com>

---
+++ cypress/component/Notification.cy.tsx
@@ -0,0 +1,120 @@
+// Notification.cy.tsx Notification component tests
+import { Notification, NotificationProps } from "../../src/indexLib";
+
+export const InlineNotification = ({
+  role,
+  hideCloseButton,
+  children,
+}: NotificationProps) => {
+  return (
+    <Notification
+      role={role}
+      hideCloseButton={hideCloseButton}
+      title="Notification title"
+      subtitle="Subtitle goes here"
+      data-id="inline-notification"
+      aria-label="Close"
+    >
+      {children}
+    </Notification>
+  );
+};
+
+const notification = '[data-id="inline-notification"]';
+const notificationTitle = '[data-id="inline-notification--title"]';
+const notificationSubTitle = '[data-id="inline-notification--subTitle"]';
+const notificationCloseButton = '[data-id="inline-notification--closeButton"]';
+const notificationIcon = '[data-id="inline-notification--icon"]';
+
+// Basic tests to see if notification renders correctly
+describe("Inline Notification", () => {
+  it("renders working notification, title, subtitle.", () => {
+    cy.mount(<InlineNotification role="info" />);
+    cy.get(notification).should("exist");
+    cy.get(notificationTitle).should("contain.text", "Notification title");
+    cy.get(notificationSubTitle).should("contain.text", "Subtitle goes here");
+  });
+
+  // Children check
+  it("renders children", () => {
+    cy.mount(
+      <InlineNotification role="info">
+        <p data-id="inline-notification--children">Children</p>
+      </InlineNotification>
+    );
+    cy.get('[data-id="inline-notification--children"]').should(
+      "contain.text",
+      "Children"
+    );
+  });
+
+  // Icon check
+  it("renders icon", () => {
+    cy.mount(<InlineNotification role="info" />);
+    cy.get(notificationIcon).should("exist");
+  });
+
+  // Role check
+  it("renders correct role attributes", () => {
+    cy.mount(<InlineNotification role="info" />);
+    cy.get(notification)
+      .should("have.attr", "role")
+      .and("to.have.string", "info");
+    cy.mount(<InlineNotification role="success" />);
+    cy.get(notification)
+      .should("have.attr", "role")
+      .and("to.have.string", "success");
+    cy.mount(<InlineNotification role="warning" />);
+    cy.get(notification)
+      .should("have.attr", "role")
+      .and("to.have.string", "warning");
+    cy.mount(<InlineNotification role="alert" />);
+    cy.get(notification)
+      .should("have.attr", "role")
+      .and("to.have.string", "alert");
+  });
+
+  // Close button
+  it("renders close button", () => {
+    cy.mount(<InlineNotification role="info" />);
+    cy.get(notificationCloseButton).should("exist");
+  });
+
+  // Close button click
+  it("closes notification on close button click", () => {
+    cy.mount(<InlineNotification role="info" />);
+    cy.get(notificationCloseButton).click();
+    cy.get(notification).should("not.exist");
+  });
+
+  // Hide close button
+  it("hides close button", () => {
+    cy.mount(<InlineNotification hideCloseButton />);
+    cy.get(notificationCloseButton).should("not.exist");
+  });
+
+  // Classname check
+  it("renders correct classnames", () => {
+    cy.mount(<InlineNotification role="info" />);
+    cy.get(notification)
+      .should("have.attr", "class")
+      .and("to.have.string", "root");
+    cy.get(notificationTitle)
+      .should("have.attr", "class")
+      .and("to.have.string", "title");
+    cy.get(notificationSubTitle)
+      .should("have.attr", "class")
+      .and("to.have.string", "subtitle");
+    cy.get(notificationCloseButton)
+      .should("have.attr", "class")
+      .and("to.have.string", "closeButton");
+  });
+
+  // Aria attribute for close button
+  it("renders correct aria attributes", () => {
+    cy.mount(<InlineNotification role="info" aria-label="Close" />);
+    cy.get(notificationCloseButton)
+      .should("have.attr", "aria-label")
+      .and("to.have.string", "Close");
+  });
+});
