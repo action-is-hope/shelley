5cf80a81df88eb4e7ffce8a79cab7d37622746b2
Vaiga <vaiginta@gmail.com>
2023-08-22 14:48:37 +0100

    tests

--- src/SpeedDial/__tests__/SpeedDial.test.tsx
+++ src/SpeedDial/__tests__/SpeedDial.test.tsx
@@ -1,6 +1,17 @@
 import renderer from "react-test-renderer";
 import { SpeedDial } from "../SpeedDial";
 
+jest.mock("../speedDial.st.css", () => {
+  const originalSt: object = jest.requireActual("../speedDial.st.css");
+
+  return {
+    ...originalSt,
+    st: () => {
+      return `speedDial___mock`;
+    },
+  } as object;
+});
+
 describe("SpeedDial", () => {
   it("renders correctly when closed", () => {
     const tooltipTitleMock = "Tooltip Test";
