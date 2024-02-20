916c47d38cf72f36ebac25edb22fca88fd93630a
Mark Napthine <mnapthine@gmail.com>
2024-02-20 15:57:42 +0000

    Split the button styles into Base

--- src/styles/shelley/index.ts
+++ src/styles/shelley/index.ts
@@ -5,12 +5,14 @@ import { classes as dark } from "./themes/dark.st.css";
 import { classes as light } from "./themes/light.st.css";
 
 // = Foundation Components
+import "./buttonBase.st.css";
 import "./actionButton.st.css";
-import "./blockquote.st.css";
+import "./iconButton.st.css";
 import "./button.st.css";
+import "./blockquote.st.css";
 import "./comboBoxMultiSelectItem.st.css";
 import "./helpText.st.css";
-import "./iconButton.st.css";
+
 import "./grid.st.css";
 import "./listBoxOption.st.css";
 import "./menuItem.st.css";
